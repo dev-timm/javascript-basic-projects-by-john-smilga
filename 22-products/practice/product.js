df504abdfc9246453aa20ebec700e17cb29afc23
dev-timm <timm.kuemmeth@gmail.com>
2024-06-14 17:53:46 +0200

    fix project 22

--- 22-products/practice/product.js
+++ 22-products/practice/product.js
@@ -3,27 +3,34 @@ const url = 'https://www.course-api.com/javascript-store-single-product';
 
 const fetchProduct = async () => {
   try {
-    productDOM.innerHTML = `<h4 class="product-loading">Loading...</h4>`;
+    productDOM.innerHTML = '<h4 class="product-loading">Loading... </h4>';
     // console.log(window.location.search);
     const params = new URLSearchParams(window.location.search);
     const id = params.get('id');
 
-    const resp = await fetch(`${url}?id=${id}`);
-    const data = await resp.json();
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json();
     return data;
   } catch (error) {
-    productDOM.innerHTML = `<p class="error">There was a problem loading the product. Please try again later</p>`;
+    productDOM.innerHTML =
+      '<p class="error">There was a problem loading the product. Please try again later </p>';
   }
 };
 
 const displayProduct = (product) => {
-  // company, colors, description , name:title, price, url:img
-  const { company, colors, description, name: title, price } = product.fields;
-  const { url: img } = product.fields.image[0];
+  // company, colors, description, name:title, price, image(url:img)
+  const {
+    company,
+    colors,
+    description,
+    name: title,
+    price,
+    image,
+  } = product.fields;
+  const { url: img } = image[0];
   document.title = title.toUpperCase();
 
   // colors
-  console.log(colors);
   const colorsList = colors
     .map((color) => {
       return `<span class="product-color" style="background: ${color}"></span>`;
@@ -31,19 +38,20 @@ const displayProduct = (product) => {
     .join('');
 
   productDOM.innerHTML = `<div class="product-wrapper">
-        <img src="${img}" class="img" alt="title" />
+        <img src="${img}" class="img" alt="${title}" />
         <div class="product-info">
           <h3>${title}</h3>
           <h5>${company}</h5>
-          <span>$${price / 100}</span>
+          <span>${price / 100}</span>
           <div class="colors">
-          ${colorsList}
+            ${colorsList}
+            
           </div>
+          <p>
+           ${description}
+          </p>
+          <button class="btn">add to cart</button>
         </div>
-        <p>
-          ${description}
-        </p>
-        <button class="btn">add to cart</button>
       </div>`;
 };
 
